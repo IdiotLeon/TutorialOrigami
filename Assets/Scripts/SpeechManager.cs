32fe2cc056b77a9743a8ef38cfb9ea010e2e210b
Leon <yanglyu.leon.7@gmail.com>
2019-07-19 22:16:42 -0700

    tutorial: Adding voice recognition
    chapter 04, Voice

---
+++ Assets/Scripts/SpeechManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class SpeechManager : MonoBehaviour
+{
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keywords.Add("Reset world", () =>
+        {
+            // to call the OnReset method on every descent object
+            this.BroadcastMessage("OnReset");
+        });
+
+        keywords.Add("Drop Sphere", () =>
+        {
+            var focusObject = GazeGestureManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                // to call the OnDrop method on just the focused object
+                focusObject.SendMessage("OnDrop", SendMessageOptions.DontRequireReceiver);
+            }
+        });
+
+        // to tell the KeywordRecognizer about our keywords
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        // to register a callback for the KeywordRecognizer and start recognizing
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+}
--- Assets/Scripts/SphereCommands.cs
+++ Assets/Scripts/SphereCommands.cs
@@ -2,6 +2,14 @@
 
 public class SphereCommands : MonoBehaviour
 {
+    Vector3 originalPosition;
+
+    private void Start()
+    {
+        // to grab the original local position of the sphere when the app starts
+        originalPosition = this.transform.localPosition;
+    }
+
     // called by GazeGestureManager when the user performs a "Select" gesture
     void OnSelect()
     {
@@ -11,5 +19,27 @@ public class SphereCommands : MonoBehaviour
             var rigidbody = this.gameObject.AddComponent<Rigidbody>();
             rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
         }
+    }
+
+    // called by SpeechManager when the user says "Reset world" command
+    void OnReset()
+    {
+        // to remove it to disable physics, if the sphere has a Rigidbody component
+        var rigidbody = this.GetComponent<Rigidbody>();
+        if (rigidbody != null)
+        {
+            rigidbody.isKinematic = true;
+            Destroy(rigidbody);
+        }
+
+        // to put the sphere back into its original local position
+        this.transform.localPosition = originalPosition;
+    }
+
+    // called by SpeechManager when the user says the "Drop sphere" command
+    void OnDrop()
+    {
+        // to do the same logic as a "Select" gesture
+        OnSelect();
     }
 }
