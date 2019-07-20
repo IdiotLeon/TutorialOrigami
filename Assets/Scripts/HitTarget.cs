2e83836a1cd89bdd742126333051fb6ade81a743
Leon <yanglyu.leon.7@gmail.com>
2019-07-20 11:30:38 -0700

    tutorial: Hitting the target
    chapter 1.7, Holographic fun

---
+++ Assets/Scripts/HitTarget.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitTarget : MonoBehaviour
+{
+    // these public fields become settable properties in the Unity editor
+    public GameObject underworld;
+    public GameObject objectToHide;
+
+    // Occurs when this object starts colliding with another object
+    void OnCollisionEnter(Collision collision)
+    {
+        // to hide the stage and show the underworld
+        objectToHide.SetActive(false);
+        underworld.SetActive(true);
+
+        // to disable "Spatial Mapping" to let the spheres enter the underworld
+        SpatialMapping.Instance.MappingEnabled = false;
+    }
+}
